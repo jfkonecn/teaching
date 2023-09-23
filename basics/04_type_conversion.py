859db2aed3fc6d2f0a028d9c9868f86b669c2c04
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-09-23 12:13:15 -0400

    a

---
+++ basics/04_type_conversion.py
@@ -0,0 +1,12 @@
+birth_year = input('Enter your birth year: ')
+# see what happens if you don't convert the input to an integer
+age = 2023 - int(birth_year)
+print(f'You are {age} years old')
+
+
+# you have other options for converting data
+# int()
+# float()
+# bool()
+# str()
+# you can also use the type() function to see what type of data you have
---
+++ basics/06_string.py
@@ -0,0 +1,8 @@
+# this is an object and it has properties and methods
+course = "Python for Beginners"
+# does not change the original string
+print(course.upper())
+
+print(course.lower())
+print(course.find("y"))
+print(course.replace("Beginners", "Absolute Beginners"))
