1e733ff47c7341ee39598aaacb7662656807a09d
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-09-23 12:21:21 -0400

    a

---
+++ basics/10_if_statement.py
@@ -0,0 +1,14 @@
+temperature = 35
+if temperature > 30:
+    # point out signifigance of indentation
+    # point out single quotes vs double quotes
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("It's a nice day")
+elif temperature > 10:
+    print("It's a bit cold")
+else:
+    print("It's cold")
+print("Done")
+
